237da2f36c868aca90757cc1b1c6fcffa6dea5b4
AndreSTich <andreytihonov447@gmail.com>
2025-09-17 11:57:12 +0300

    Lemonade app

--- Lemonade/app/src/main/java/com/example/lemonade/MainActivity.kt
+++ Lemonade/app/src/main/java/com/example/lemonade/MainActivity.kt
@@ -1,16 +1,22 @@
-package com.example.diceroller
+package com.example.lemonade
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -20,23 +26,25 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.example.diceroller.ui.theme.DiceRollerTheme
+import com.example.lemonade.ui.theme.LemonadeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContent {
-            DiceRollerTheme {
+            LemonadeTheme {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    DiceRollerApp()
+                    LemonadeApp()
                 }
             }
         }
@@ -45,31 +53,43 @@ class MainActivity : ComponentActivity() {
 
 @Preview
 @Composable
-fun DiceRollerApp() {
-    DiceWithButtonAndImage(modifier = Modifier
+fun LemonadeApp() {
+    LemonadeButtonAndImage(modifier = Modifier
         .fillMaxSize()
         .wrapContentSize(Alignment.Center)
     )
 }
 
 @Composable
-fun DiceWithButtonAndImage(modifier: Modifier = Modifier) {
-    var result by remember { mutableStateOf( 1) }
+fun LemonadeButtonAndImage(modifier: Modifier = Modifier) {
+    var result by remember { mutableStateOf( 0) }
+    result = result % 4
     val imageResource = when(result) {
-        1 -> R.drawable.dice_1
-        2 -> R.drawable.dice_2
-        3 -> R.drawable.dice_3
-        4 -> R.drawable.dice_4
-        5 -> R.drawable.dice_5
-        else -> R.drawable.dice_6
+        0 -> R.drawable.lemon_tree
+        1 -> R.drawable.lemon_squeeze
+        2 -> R.drawable.lemon_drink
+        3 -> R.drawable.lemon_restart
+        else -> R.drawable.lemon_restart
+    }
+    val stringResource = when(result){
+        0 -> R.string.lemon_tree
+        1 -> R.string.lemon
+        2 -> R.string.glass_of_lemonade
+        3 -> R.string.empty_glass
+        else -> R.string.empty_glass
     }
     Column(modifier = modifier, horizontalAlignment = Alignment.CenterHorizontally) {
-        Image(painter = painterResource(imageResource), contentDescription = result.toString())
-        Spacer(modifier = Modifier.height(16.dp))
         Button(
-            onClick = { result = (1..6).random() },
+            onClick = { result++ },
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Color(0xFF11eed4),
+            ),
+            shape = RoundedCornerShape(18.dp)
         ) {
-            Text(text = stringResource(R.string.roll), fontSize = 24.sp)
+            Image(painter = painterResource(imageResource), contentDescription = result.toString())
         }
+        Spacer(modifier = Modifier.height(20.dp))
+        Text(text = stringResource(stringResource), fontSize = 20.sp)
     }
+
 }
